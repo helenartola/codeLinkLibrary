d663d1205d38c1e19c1e146d51c5ada5e51f1aa8
TnyBertran <bertran94@gmail.com>
2023-12-11 17:32:54 +0100

    Base del Servidor

---
+++ app.js
@@ -0,0 +1,50 @@
+//importamos .env (variables de entorno)
+import 'dotenv/config';
+
+
+
+//imporrtamos express
+import express from 'express';
+
+//creacion del servidor con express
+const app = express();
+
+//middleware (peticiones) qe muestra el metodo y la ruta (endpoint) de la peticion
+app.use ((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+
+    //pasamos al siguiente middleware
+    next();
+});
+
+
+//middleware que devuelve los posts de la base de datos
+app.get('/posts', (req, res) => {
+    res.send({
+        status: 'ok',
+        message: 'Aqui tines el listado de posts',
+    });
+}); 
+
+
+//middleware que crea post en base de datos
+app.post('/posts', (req, res) => {
+    res.status(201).send({
+        status: 'ok',
+        message: 'Post creado',
+    });
+}); 
+
+//middleware de ruta no encontrada
+app.use((req, res) => {
+    res.status(404).send({
+        status:'error',
+        message: 'Ruta no encontrada',
+    });
+});
+
+
+//puerto desde donde se escuchan peticiones
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor escuchando en http://localhost:${process.env.PORT}`)
+});
