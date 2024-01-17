e58c03b3ce131c8e33f16c04f33e8bc95ccce0d7
annalona <anna.ln@hotmail.com>
2024-01-17 15:45:27 +0100

    Generar error
    Generar un error específico, no sólo console log.

--- DB/getPool.js
+++ DB/getPool.js
@@ -1,36 +1,35 @@
-import dotenv from 'dotenv'; //Importo dependendecia DOTENV que me sirve para acceder al archivo .env
-import mysql from 'mysql2'; //Importo dependencia de mysql para manejo de base de datos
+import dotenv from 'dotenv';
+import mysql from 'mysql2/promise';
+import { generateError } from '../helpers.js';
 
-dotenv.config(); //inicializo la dependencia de dotenv, lo que hace es agregar a las variables globales (process) las variables de entorno del archivo .env
+dotenv.config();
 
-const { DB_HOST, DB_USER, DB_NAME, DB_PASS, DB_PORT } = process.env; //Desestructuro el objeto creado en las variables de entorno que se encuentran en mi archivo .env
+const { DB_HOST, DB_USER, DB_NAME, DB_PASS, DB_PORT } = process.env;
 
-let pool; //Declaro la variable para mi POOL de conexiones (NO LA INICIALIZO)
+let pool;
 
 const getConnection = async () => {
-  //Inicia mi función getPool que devuelve el pool a crear
   try {
-    //Inicia TRY
+    // Verifico si el pool aún no ha sido inicializado
     if (!pool) {
-      //Verifico que el pool no esté inicializado anteriormente
-
+      // Creo el pool si aún no ha sido inicializado
       pool = mysql.createPool({
-        //Comienzo a crear el pool mediante MYSQL y le envío un objeto
-        host: DB_HOST, //envio el host para la conexión
-        user: DB_USER, //envio el user
-        password: DB_PASS, //envio el password
+        host: DB_HOST,
+        user: DB_USER,
+        password: DB_PASS,
         port: DB_PORT,
-        connectionLimit: 10, //determino la cantidad máxima de conexiones (10 por poner 10)
-        database: DB_NAME, //determino la base a la cual conectarme
-        timezone: 'Z', //Z para horario UTC (horario global)
-      }); //cierro el createPool
-    } //cierro el if donde valido si no está inicializado el pool
+        connectionLimit: 10,
+        database: DB_NAME,
+        timezone: 'Z',
+      });
+    }
 
-    return await pool.getConnection(); //devuelvo el pool ya creado
+    // Devuelvo una conexión del pool
+    return await pool.getConnection();
   } catch (error) {
-    //catcheo el error, recibo error como variable
-    console.error(error); //muestro el error
-  } //finaliza el trycatch
-}; //finaliza la función
+    // En caso de error, lanzo una excepción con un mensaje descriptivo
+    throw generateError('Acceso denegado a la base de datos, 401.');
+  }
+};
 
-export default getConnection; //exporto por default la función
+export default getConnection;
