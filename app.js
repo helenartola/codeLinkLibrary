9d4196c5d4f106f4c01fb17c028239c5afd4fb46
annalona <anna.ln@hotmail.com>
2024-01-18 11:39:02 +0100

    userExists
    Llamada a userExists después de authUser y antes del controlador.

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-//importamos .env (variables de entorno)
+// Importamos .env (variables de entorno)
 import 'dotenv/config';
-//importamos express
+
+// Importamos express
 import express from 'express';
-//middleware morgan que imprime en terminal el tiempo de petición
+
+// Middleware morgan que imprime en terminal el tiempo de petición
 import morgan from 'morgan';
 
-// Importamos las funciones que hemos creado en el users.js
+// Importamos las funciones que hemos creado en users.js
 import {
   newUserController,
   getAllUsersController,
@@ -24,35 +26,37 @@ import {
   likePostController
 } from './controllers/posts.js';
 
-// Importamos las funciones que hemos creado en auth.js
+// Importamos el middleware de autenticación
 import { authUser } from './middlewares/auth.js';
 
-//creacion del servidor con express
+// Importamos el middleware para verificar la existencia del usuario
+import { userExists } from './middlewares/userExists.js';
+
+// Creacion del servidor con express
 const app = express();
-//le decimos a express que intente procesar los datos que se envien en la petición en formato json.
+
+// Le decimos a express que intente procesar los datos que se envíen en la petición en formato json.
 app.use(express.json());
+
+// Middleware morgan para imprimir en la terminal el tiempo de petición
 app.use(morgan('dev'));
 
-//Rutas de usuarios (cada ruta tendrá que gestionar un controlador)
-//Las funciones newUserController, getUser... las creamos dentro del archivo users.js de la carpeta controllers. Y luego las importamos aquí.
-app.post('/user', newUserController);
-app.get('/users', authUser, getAllUsersController);
-app.get('/user/:id', authUser, getUserController);
-app.post('/login', loginController);
-app.delete('/user/:id', authUser, deleteUserController);
+// Rutas de usuarios
+app.post('/user', newUserController); // Crear un nuevo usuario
+app.get('/users', authUser, userExists, getAllUsersController); // Obtener todos los usuarios autenticados
+app.get('/user/:id', authUser, userExists, getUserController); // Obtener un usuario específico
+app.post('/login', loginController); // Iniciar sesión
+app.delete('/user', authUser, userExists, deleteUserController); // Eliminar un usuario y manejar el token
 
-//Rutas de posts
-app.get('/posts', getPostsController); //Devuelve todos los posts de todos los usuarios
-app.get('/posts/:id', getPostsByUserController); //Devuelve todos los posts de un único usuario
-app.get('/user/:userId/post/:postId', getPostByUserController); //Nos devuelve un post concreto de un usuario en concreto
-app.post('/', authUser, newPostController); //Crea un post
-app.delete('/post/:postId', authUser, deletePostController); //Borramos un post concreto de un usuario concreto
+// Rutas de posts
+app.get('/posts', getPostsController); // Obtener todos los posts de todos los usuarios
+app.get('/posts/:id', getPostsByUserController); // Obtener todos los posts de un usuario
+app.get('/user/:userId/post/:postId', getPostByUserController); // Obtener un post específico de un usuario
+app.post('/', authUser, userExists, newPostController); // Crear un nuevo post
+app.delete('/post/:postId', authUser, userExists, deletePostController); // Eliminar un post y manejar el token
+app.post('/post/:postId/like', authUser, userExists, likePostController); // Dar "like" a un post y manejar el token
 
-// Nuevas rutas para "Likes"
-//app.post('/post/:postId/like', authUser, postExists,  likePostController); 
-app.post('/post/:postId/like', authUser, likePostController); 
-
-//middleware de ruta no encontrada
+// Middleware de ruta no encontrada
 app.use((req, res) => {
   res.status(404).send({
     status: 'error',
@@ -60,7 +64,7 @@ app.use((req, res) => {
   });
 });
 
-//middleware de gestión de errores
+// Middleware de gestión de errores
 app.use((error, req, res, next) => {
   console.error(error);
   res.status(error.httpStatus || 500).send({
@@ -69,7 +73,7 @@ app.use((error, req, res, next) => {
   });
 });
 
-//puerto desde donde se escuchan peticiones
+// Puerto desde donde se escuchan peticiones
 app.listen(8000, () => {
   console.log(`Servidor escuchando en http://localhost:8000`);
 });
